41796c010cdcb676cb3dbd8ceabc51346510631d
pirojohek <pirojochek01@gmail.com>
2025-12-09 20:15:50 +0700

    upd: updated endpoint name

--- src/main/java/by/pirog/ReverseGanttChart/controller/StudentTaskStatusController.java
+++ src/main/java/by/pirog/ReverseGanttChart/controller/StudentTaskStatusController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/taskStatus")
+@RequestMapping("/api/studentTaskStatus")
 public class StudentTaskStatusController {
 
     private final StudentTaskStatusService taskStatusService;
